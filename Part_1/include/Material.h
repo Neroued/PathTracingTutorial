5374107c238b93290e0aaff0adea635b481aa437
Neroued <neroued@gmail.com>
2025-03-27 18:25:54 +0100

    Part 1附录部分

---
+++ Part_1/include/Material.h
@@ -0,0 +1,8 @@
+#pragma once
+
+#include <QVector3D>
+
+struct Material
+{
+    
+};
---
+++ Part_1/include/ptScene.h
@@ -0,0 +1,55 @@
+#pragma once
+
+#include <Primitive.h>
+#include <Material.h>
+
+#include <QOpenGLWidget>
+#include <QOpenGLFunctions_4_5_Core>
+#include <QOpenGLShaderProgram>
+#include <QOpenGLVertexArrayObject>
+#include <QOpenGLBuffer>
+
+#include <vector>
+
+class ptScene : public QOpenGLWidget, protected QOpenGLFunctions_4_5_Core
+{
+public:
+    ptScene(QWidget *parent = nullptr);
+    ~ptScene();
+
+    void loadScene(); // 加载场景
+
+protected:
+    void initializeGL() override;
+    void resizeGL(int w, int h) override;
+    void paintGL() override;
+
+private:
+    void compileShaders(); // 编译所需的着色器
+
+    void computeShaderPass(); // 计算pass，进行路径追踪的pass
+    void mixShaderPass();     // 混合pass，将新的结果与之前的混合
+    void renderShaderPass();  // 渲染pass，展示计算结果的pass
+
+    void uploadScene(); // 上传场景信息
+
+    void initializeQuad();                                                   // 初始化全屏四边形
+    void createTexture(GLuint *texture, int width, int height, GLuint unit); // 创建材质
+
+private:
+    QOpenGLShaderProgram *m_computeProgram; // 计算着色器
+    QOpenGLShaderProgram *m_mixProgram;     // 混合着色器
+    QOpenGLShaderProgram *m_renderProgram;  // 展示结果的着色器
+
+    QOpenGLVertexArrayObject m_screenVAO; // 用于屏幕的vao和vbo
+    QOpenGLBuffer m_screenVBO;
+
+    GLuint m_computeTexture; // 存放计算结果的材质
+    GLuint m_imageTexture;   // 存储历史结果的材质
+
+    int m_width, m_height;
+    GLuint m_frameCount = 0;
+
+    std::vector<Primitive> m_primitives;
+
+};
